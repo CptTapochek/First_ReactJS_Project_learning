c149d555b19f5851af62fc418a8683ad6d6a29fc
Bozu Andrei <93885458+CptTapochek@users.noreply.github.com>
2022-03-05 16:35:21 +0200

    project init

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <header className="header">
+            <img src="https://duckworthbooks.com/wp-content/uploads/2017/06/logo-tv-logo.png"></img>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,25 @@
+const Profile = () => {
+    return (
+        <div className="content">
+            <div>
+                <img src="https://wallpaperforu.com/wp-content/uploads/2020/07/neon-wallpaper-200702140825151366x768.jpg"></img>
+            </div>
+            <div>
+                avatar + description
+            </div>
+            <div>
+                My posts
+                <div>new post</div>
+            </div>
+
+            <div>
+                <div>post 1</div>
+                <div>post 2</div>
+                <div>post 3</div>
+            </div>
+
+        </div>
+    );
+}
+
+export default Profile;
